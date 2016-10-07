2d39d9fa6ce771556f781dd988c3335d373ded64
mal-wood <mallorywood413@gmail.com>
2016-10-06 21:29:32 -0700

    Add route for registration

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,22 @@
 require 'bcrypt'
 
 class User < ActiveRecord::Base 
+	validates :first_name, :last_name, :email, presence: true
+	validates :email, uniqueness: true
 
+  include BCrypt
+
+  def password
+    @password ||= Password.new(password_hash)
+  end
+
+  def password=(new_password)
+    @password = Password.create(new_password)
+    self.password_hash = @password
+  end
+
+  def self.athenticate
+  	# user = self.find_by(email: email)
+  	# if user && user.
+  end
 end 
