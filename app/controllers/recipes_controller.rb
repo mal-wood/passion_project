508f635c98c52c4329699d8ce92e338e66ef258a
mal-wood <mallorywood413@gmail.com>
2016-10-17 16:26:57 -0700

    Add favicon

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,11 +1,12 @@
-get '/recipes' do 
+get '/user/:id/recipes' do 
 	@user = User.find(session[:id])
 	@recipes = @user.recipes
 	erb :'recipes/index'
 end 
 
-get '/recipes/new' do 
+get '/user/:user_id/recipes/new' do 
 	@recipe_url = params[:recipe_url]
+	p @recipe_url
 	@scraped_recipe = RecipeScraper.parse_url(@recipe_url)
 	@recipe = Recipe.create(title: @scraped_recipe[0], ingredients: @scraped_recipe[1], directions: @scraped_recipe[2], user_id: session[:id])
 	erb :'recipes/new'
@@ -15,7 +16,7 @@ post '/recipes' do
 	redirect '/recipes'
 end 
 
-get '/recipes/:id' do
-	@recipe = Recipe.find(params[:id]) 
-	erb :'recipes/show'
+get '/user/:user_id/recipes/:id' do
+	@recipe = Recipe.find(params[:id])
+		erb :'recipes/show'
 end 
