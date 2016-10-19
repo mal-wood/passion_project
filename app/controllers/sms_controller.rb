d6ac0d6fed4e7ac43f8f04c41086ca2871704d56
mal-wood <mallorywood413@gmail.com>
2016-10-18 21:10:12 -0700

    Implement twilio successfully

--- app/controllers/sms_controller.rb
+++ app/controllers/sms_controller.rb
@@ -1,4 +1,7 @@
-# post '/sms/:recipe_id' do 
-# 	@recipe = Recipe.find(params[:recipe_id])
-# 	Notifier.send_sms_notification("+17248126820", @recipe.ingredients)
-# end
+post '/sms/:recipe_id' do 
+	@recipe = Recipe.find(params[:recipe_id])
+	@recipe_string = @recipe.ingredients.map(&:inspect).join(', ')
+	Notifier::send_sms_notification("+17248126820", @recipe_string)
+	p @recipe.ingredients
+	redirect '/user/:id/recipes'
+end
