0e53774e2dca29ce879a891494aad7f6a5c327a5
mal-wood <mallorywood413@gmail.com>
2016-10-20 21:59:25 -0700

    Add edit functionality

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -16,10 +16,31 @@ post '/recipes' do
 	redirect '/recipes'
 end 
 
-get '/user/:user_id/recipes/:id' do 
+get '/user/:user_id/recipes/:id/edit' do 
 	@user = User.find(params[:user_id])
 	@recipe = Recipe.find(params[:id])
+	erb :'recipes/edit'
+end 
+
+get '/user/:user_id/recipes/:id' do 
+	@user = User.find(params[:user_id])
+	@recipe
+	p "88888"
+	@recipe = Recipe.find(params[:id])
 	erb :'recipes/show'
+end
+
+put '/user/:user_id/recipes/:id' do
+	@recipe = Recipe.find(params[:id])
+	p params[:recipe]
+	p "88888"
+	@recipe.update_attributes(params[:recipe])
+	@user = User.find(params[:user_id])
+	if @recipe.save
+		redirect "/user/#{@user.id}/recipes/#{@recipe.id}"
+	else
+		erb :'recipes/edit'
+	end
 end
 
 delete '/user/:user_id/recipes/:id' do 
